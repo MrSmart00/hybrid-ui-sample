4b5576d00ebbc07a957e8a400459332ab182aea9
Hiroya Hinomori <nexearth@gmail.com>
2021-10-23 22:57:53 +0900

    Add: SwiftPM files

---
+++ App/iOS/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  iOS
+//
+//  Created by hiroya-hinomori on 2021/10/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = .init(frame: UIScreen.main.bounds)
+        window?.rootViewController = .init()
+        window?.makeKeyAndVisible()
+        return true
+    }
+
+}
+
